83cf503bfb1225adac23c61f09ebd0fa0cf8f158
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-05-05 21:15:05 +0800

    [Develop #1 - 6] Engine Namespace Set

--- Engine.h
+++ Engine.h
@@ -4,18 +4,18 @@
 
 #ifndef INC_2018DATASTRUCTUREBIGWORK_ENGINE_H
 #define INC_2018DATASTRUCTUREBIGWORK_ENGINE_H
+namespace myAlgorithm {
+    class Engine {
+        template<class T>
+        class BPlusTree {
+            const int NUMBER_OF_SON = 10; // 结点最多的儿子数目
+            const int SIZE_BLOCK = 4096;
+            struct Node {
+                Node *prev, *next;
 
-class Engine {
-    template <class T>
-    class BPlusTree{
-        const int NUMBER_OF_SON = 10; // 结点最多的儿子数目
-        const int SIZE_BLOCK = 4096;
-        struct Node{
-            Node* prev, *next;
-
+            };
         };
     };
-};
-
+}
 
 #endif //INC_2018DATASTRUCTUREBIGWORK_ENGINE_H
