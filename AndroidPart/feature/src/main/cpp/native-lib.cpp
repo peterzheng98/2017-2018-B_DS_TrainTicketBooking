203c47632d8fb99b34260b246300ac148d80e83a
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-05-08 14:54:23 +0800

    [Develop #2 - 1] Android UI(Stage 1)

--- AndroidPart/feature/src/main/cpp/native-lib.cpp
+++ AndroidPart/feature/src/main/cpp/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
+extern "C" JNIEXPORT jstring
+
+JNICALL
 Java_group_peterzheng_trainticketbookingsystem_feature_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Welcome to Train Ticket Booking System!";
     return env->NewStringUTF(hello.c_str());
 }
--- AndroidPart/feature/src/main/java/group/peterzheng/trainticketbookingsystem/feature/MainActivity.kt
+++ AndroidPart/feature/src/main/java/group/peterzheng/trainticketbookingsystem/feature/MainActivity.kt
@@ -5,10 +5,21 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+
 class MainActivity : AppCompatActivity() {
+    var ipAddressT : TextView? = null
+    var ipPortT : TextView? = null
+    var testConnB : Button? = null
+    var loginOfflineB : Button? = null
+    var loginOnlineB : Button? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +31,32 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
 
-    // Example of a call to a native method
-    sample_text.text = stringFromJNI()
+        // Example of a call to a native method
+//        sample_text.text = stringFromJNI()
+        // 隐藏不需要的模块
+        textStatus.visibility = TextView.INVISIBLE;
+        testProgress.visibility = ProgressBar.INVISIBLE;
+
+        initView()
+
     }
 
+    // 绑定各个控件
+    fun initView(){
+        ipAddressT = findViewById<TextView>(R.id.ipAddr)
+        ipPortT = findViewById<TextView>(R.id.ipPort)
+        testConnB = findViewById<Button>(R.id.testConn)
+        loginOnlineB = findViewById<Button>(R.id.loginOnline)
+        loginOfflineB = findViewById<Button>(R.id.loginOffline)
+        //绑定按钮监听
+        loginOfflineB!!.setOnClickListener{
+            textStatus.visibility = TextView.VISIBLE
+            textStatus!!.setText("正在加载数据登录系统")
+            testProgress.visibility = ProgressBar.VISIBLE
+        }
+        testConnB!!.setOnClickListener{
+        }
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -34,7 +67,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
