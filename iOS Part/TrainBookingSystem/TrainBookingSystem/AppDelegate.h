82fa7db320e68a3b6bacc090273912df97c1b752
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-05-12 23:13:18 +0800

    [Develop 2B - 0] iOS Part Initalized

---
+++ iOS Part/TrainBookingSystem/TrainBookingSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrainBookingSystem
+//
+//  Created by Peter Zheng on 2018/05/12.
+//  Copyright © 2018年 Peter Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS Part/TrainBookingSystem/TrainBookingSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrainBookingSystem
+//
+//  Created by Peter Zheng on 2018/05/12.
+//  Copyright © 2018年 Peter Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
