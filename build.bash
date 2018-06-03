c723dfba140acf0728dea22d4f46517bbd061847
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-06-03 08:06:15 +0800

    [Test]

--- build.bash
+++ build.bash
@@ -1,2 +1,13 @@
 mkdir tmp
+cd tmp
+rm -rf *
+touch userName.dat create file
+touch test.datl create file
+touch train.dat create file
+touch userTicket.dat create file
+touch user.dat create file
+touch ticketId.dat create file
+touch ticket.dat create file
+touch id.dat create file
+cd ..
 g++ main.cpp -o code -g -w -O2 -std=c++14
