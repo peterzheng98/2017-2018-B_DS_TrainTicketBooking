b54f28ad9eb705a9c21c598d6566705cb6f1afff
smallling <lhy526620793@qq.com>
2018-06-11 19:28:32 +0800

    [Develop 2A-9]Android UI

---
+++ AndroidPart/feature/src/main/java/group/peterzheng/trainticketbookingsystem/feature/javaFragment.java
@@ -0,0 +1,19 @@
+package group.peterzheng.trainticketbookingsystem.feature;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class javaFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_java, container,false);
+        return view;
+    }
+}
